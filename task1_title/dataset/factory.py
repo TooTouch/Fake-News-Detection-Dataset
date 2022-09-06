d7811f15ac19dfc2924aed92ceabf87e7bdeab5c
tootouch <wogur379@gmail.com>
2022-09-06 12:38:39 +0900

    modify args

--- task1_title/dataset/factory.py
+++ task1_title/dataset/factory.py
@@ -25,37 +25,37 @@ def extract_word_embedding(vocab_path, max_vocab_size=-1):
 
 
 
-def create_tokenizer(args):
-    if args.tokenizer == 'mecab':
-        vocab, word_embed = extract_word_embedding(vocab_path = args.vocab_path, max_vocab_size = args.max_vocab_size)
+def create_tokenizer(tokenizer, vocab_path, max_vocab_size, pretrained_name):
+    if tokenizer == 'mecab':
+        vocab, word_embed = extract_word_embedding(vocab_path = vocab_path, max_vocab_size = max_vocab_size)
         tokenizer = FNDTokenizer(vocab = vocab, tokenizer = Mecab())
-    elif args.tokenizer == 'bert':
+    elif tokenizer == 'bert':
         word_embed = None
-        tokenizer = BertTokenizer.from_pretrained(args.pretrained_name)
+        tokenizer = BertTokenizer.from_pretrained(pretrained_name)
 
     return tokenizer, word_embed 
 
 
-def create_dataset(args, split, tokenizer):
+def create_dataset(modelname, data_path, split, tokenizer, max_word_len, max_sent_len, use_saved_data):
     dataset = FNDDataset(
-        modelname      = args.modelname,
-        datadir        = args.data_path,
+        modelname      = modelname,
+        datadir        = data_path,
         split          = split,  
         tokenizer      = tokenizer, 
-        max_word_len   = args.max_word_len, 
-        max_sent_len   = args.max_sent_len,
-        use_saved_data = args.use_saved_data
+        max_word_len   = max_word_len, 
+        max_sent_len   = max_sent_len,
+        use_saved_data = use_saved_data
     )
 
     return dataset
 
 
-def create_dataloader(args, dataset, shuffle=False):
+def create_dataloader(dataset, batch_size, num_workers, shuffle=False):
 
     dataloader = DataLoader(
         dataset, 
-        batch_size  = args.batch_size, 
-        num_workers = args.num_workers, 
+        batch_size  = batch_size, 
+        num_workers = num_workers, 
         shuffle     = shuffle
     )
 
