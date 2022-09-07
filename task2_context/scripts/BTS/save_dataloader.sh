33e09d36f33c19e4b665bffb53b8e4f7d85bb2bd
tootouch <wogur379@gmail.com>
2022-09-07 22:36:41 +0900

    [task2] remove scripts

--- task2_context/scripts/BTS/save_dataloader.sh
+++
@@ -1,7 +0,0 @@
-cd ../../
-
-python save_dataloader.py \
-    --modelname BTS \
-    --batch_size 128 \
-    --window_size 1 \
-    --max_word_len 512
--- task2_context/scripts/KoBERTSeg/save_dataloader.sh
+++
@@ -1,7 +0,0 @@
-cd ../../
-
-python save_dataloader.py \
-    --modelname KoBERTSeg \
-    --batch_size 128 \
-    --window_size 3 \
-    --max_word_len 512
