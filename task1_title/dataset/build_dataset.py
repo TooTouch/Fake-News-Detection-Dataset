86bdd6702669ebe58f93597323cdfb8c75d3f832
tootouch <wogur379@gmail.com>
2022-09-21 11:11:05 +0900

    [task1] modify dataset for framework

--- task1_title/dataset/build_dataset.py
+++ task1_title/dataset/build_dataset.py
@@ -4,7 +4,9 @@ import pandas as pd
 import torch
 import os
 
+import logging
 
+_logger = logging.getLogger('train')
 
 class FakeDataset(Dataset):
     def __init__(self, tokenizer):
@@ -12,27 +14,53 @@ class FakeDataset(Dataset):
         self.tokenizer = tokenizer
 
     def load_dataset(self, datadir, split, saved_data_path=False):
-        if self.saved_data_path:
-            self.data = torch.load(os.path.join(saved_data_path, f'{split}.pt'))
+        data_info = pd.read_csv(os.path.join(datadir, f'{split}_info.csv'))
+        setattr(self, 'saved_data_path', saved_data_path)
+
+        if saved_data_path:
+            _logger.info('load saved data')
+            data = torch.load(os.path.join(saved_data_path, f'{split}.pt'))
         else:
-            data_info = pd.read_csv(os.path.join(datadir, f'{split}_info.csv'))
-            
+            _logger.info('load raw data')
+                
             data = {}
             for filename in data_info.filename:
                 f = json.load(open(os.path.join(datadir,filename),'r'))
                 data[filename] = f
 
-            setattr(self, 'data_info', data_info)
+        setattr(self, 'data_info', data_info)
         setattr(self, 'data', data)
 
-    def transform(self, sent_list):
+    def transform(self):
         raise NotImplementedError
 
-    def padding(self, doc):
+    def padding(self):
         raise NotImplementedError
 
     def __getitem__(self, i):
-        raise NotImplementedError
+        if self.saved_data_path:
+            doc = {}
+            for k in self.data['doc'].keys():
+                doc[k] = self.data['doc'][k][i]
+
+            label = self.data['label'][i]
+
+            return doc, label
+        
+        else:
+            news_idx = self.data_info.iloc[i]
+            news_info = self.data[news_idx['filename']]
+        
+            # label
+            label = 1 if news_idx['label']=='fake' else 0
+        
+            # transform and padding
+            doc = self.transform(
+                title = news_info['labeledDataInfo']['newTitle'], 
+                text  = news_info['sourceDataInfo']['newsContent'].split('\n')
+            )
+
+            return doc, label
 
     def __len__(self):
         raise NotImplementedError
--- task1_title/dataset/han.py
+++ task1_title/dataset/han.py
@@ -1,18 +1,13 @@
 from .build_dataset import FakeDataset
 
 import torch
-import os
-
 
 class HANDataset(FakeDataset):
-    def __init__(self, tokenizer, max_word_len, max_sent_len, saved_data_path=False):
+    def __init__(self, tokenizer, max_word_len, max_sent_len):
         super(HANDataset, self).__init__(tokenizer=tokenizer)
 
         self.max_word_len = max_word_len
         self.max_sent_len = max_sent_len
-
-        # load data
-        self.saved_data_path = saved_data_path
 
     def transform(self, title, text):
         sent_list = [title] + text
@@ -34,32 +29,6 @@ class HANDataset(FakeDataset):
         doc = doc + [[self.tokenizer.pad_token_id] * self.max_word_len for i in range(num_pad_doc)]
             
         return doc
-
-    def __getitem__(self, i):
-
-        if self.saved_data_path:
-            doc = {}
-            for k in self.data['doc'].keys():
-                doc[k] = self.data['doc'][k][i]
-
-            label = self.data['label'][i]
-
-            return doc, label
-        
-        else:
-            news_idx = self.data_info.iloc[i]
-            news_info = self.data[news_idx['filename']]
-        
-            # label
-            label = 1 if news_idx['label']=='fake' else 0
-        
-            # transform and padding
-            doc = self.transform(
-                title = news_info['labeledDataInfo']['newsTitle'], 
-                text  = news_info['sourceDataInfo']['newsContent'].split('\n')
-            )
-
-            return doc, label
 
 
     def __len__(self):
