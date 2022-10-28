bd5aa6d29c9982f6d4ab3313f4b08fcb2f281c8a
tootouch <wogur379@gmail.com>
2022-10-28 15:20:28 +0900

    [direct exp] add build_dataset folder

--- build_dataset.sh
+++
@@ -1,7 +0,0 @@
-# task1; title
-python build_dataset.py --data train --target title --savedir ./data/task1
-python build_dataset.py --data valid --target title --savedir ./data/task1
-
-# task2: text
-python build_dataset.py --data train --target text --savedir ./data/task2
-python build_dataset.py --data valid --target text --savedir ./data/task2
---
+++ build_dataset/clickbait_direct/build.py
@@ -0,0 +1,146 @@
+from glob import glob
+import os
+import json
+import numpy as np
+import argparse
+import yaml
+import torch
+import random
+
+from tqdm.auto import tqdm
+
+def torch_seed(random_seed):
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed) # if use multi-GPU 
+    # CUDA randomness
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+    os.environ['PYTHONHASHSEED'] = str(random_seed)
+
+
+
+def update_label_info(file: dict, new_title: str) -> dict:
+    '''
+    update label information in dictionary 
+    '''
+    
+    file['labeledDataInfo'] = {
+        'newTitle': new_title,
+        'clickbaitClass': 0,
+        'referSentenceInfo': [
+            {'sentenceNo':i, 'referSentenceyn': 'N'} for i in range(len(file['sourceDataInfo']['sentenceInfo']))
+        ]
+    }
+    
+    return file
+
+
+def make_fake_title(file_list: list, savedir: str, cfg_method: dict) -> None:
+    '''
+    make fake title using selected method
+    '''
+    for file_path in tqdm(file_list):
+        
+        # source file name and category
+        category_name = os.path.basename(os.path.dirname(file_path))
+        file_name = os.path.basename(file_path)
+
+        # load source file
+        source_file = json.load(open(file_path, 'r'))
+        
+        # extract fake title
+        if cfg_method['name'] == 'random_select':
+            kwargs = {
+                'file_path':file_path,
+                'file_list':file_list
+            }
+        elif cfg_method['name'] == 'random_category_select':
+            kwargs = {
+                'file_path':file_path,
+                'category':category_name,
+                'file_list':file_list
+            }
+
+        fake_title = __import__('methods').__dict__[cfg_method['name']](**kwargs)
+        
+        # update label infomation
+        source_file = update_label_info(file=source_file, new_title=fake_title)
+        
+        # save source file
+        category_name = os.path.basename(os.path.dirname(file_path))
+        file_name = os.path.basename(file_path)
+        json.dump(
+            obj          = source_file, 
+            fp           = open(os.path.join(savedir, category_name, file_name), 'w', encoding='utf-8'), 
+            indent       = '\t',
+            ensure_ascii = False
+        )
+
+
+def make_label(file_list: list, savedir: str) -> None:
+    '''
+    make label for NonClickbait_Auto
+    '''
+    for file_path in tqdm(file_list):
+        # source file name and category
+        category_name = os.path.basename(os.path.dirname(file_path))
+        file_name = os.path.basename(file_path)
+
+        # load source file
+        source_file = json.load(open(file_path, 'r'))
+
+        # extract new title
+        new_title = source_file['sourceDataInfo']['newsTitle']
+
+        # update label information
+        source_file = update_label_info(file=source_file, new_title=new_title)
+
+        # save source file
+        category_name = os.path.basename(os.path.dirname(file_path))
+        file_name = os.path.basename(file_path)
+        json.dump(
+            obj          = source_file, 
+            fp           = open(os.path.join(savedir, category_name, file_name), 'w', encoding='utf-8'), 
+            indent       = '\t',
+            ensure_ascii = False
+        )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--yaml_config', type=str, help='config filename')
+    args = parser.parse_args()
+    
+    cfg = yaml.load(open(args.yaml_config,'r'), Loader=yaml.FullLoader)
+
+    # set seed
+    torch_seed(cfg['SEED'])
+
+    # make directory to save files
+    if cfg['BUILD'].get('METHOD',False):
+        cfg['BUILD']['savedir'] = cfg['BUILD']['savedir'] + '_' + cfg['BUILD']['METHOD']['name']
+    os.makedirs(cfg['BUILD']['savedir'], exist_ok=True)
+
+    category_list = os.listdir(cfg['BUILD']['datadir'])
+    for cat in category_list:
+        os.makedirs(os.path.join(cfg['BUILD']['savedir'], cat), exist_ok=True)    
+
+    # load file list
+    file_list = glob(os.path.join(cfg['BUILD']['datadir'], '*/*'))
+
+    # run
+    if cfg['BUILD'].get('METHOD',False):
+        make_fake_title(
+            file_list  = file_list, 
+            savedir    = cfg['BUILD']['savedir'], 
+            cfg_method = cfg['BUILD']['METHOD']
+        )
+    else:
+        make_label(
+            file_list = file_list,
+            savedir   = cfg['BUILD']['savedir']
+        )
