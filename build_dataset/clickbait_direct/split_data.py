91dfe4595e0258f6873f4955a35636fc67e6ca64
tootouch <wogur379@gmail.com>
2022-11-12 11:37:57 +0900

    [direct exp] add assert

--- build_dataset/clickbait_direct/split_data.py
+++ build_dataset/clickbait_direct/split_data.py
@@ -10,6 +10,10 @@ from sklearn.model_selection import train_test_split
 
 
 def run(datadir: str, savedir: str, split_ratio: list, method: str, seed: int) -> None:
+    folders = os.listdir(os.path.join(datadir))
+    assert 'Clickbait_Auto' in folders, 'Clickbait_Auto folder does not exist'
+    assert 'NonClickbait_Auto' in folders, 'NonClickbait_Auto folder does not exist'
+
     task1 = glob(os.path.join(datadir,'*/*/*'))
     task1 = [f for f in task1 if 'Clickbait_Direct' not in f]
 
