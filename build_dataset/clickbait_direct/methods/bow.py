82647912350257974cf1bfd244a18c3227788f0f
Backbone451 <gunho_no@korea.ac.kr>
2022-11-26 23:25:09 +0900

    fix error

--- build_dataset/clickbait_direct/methods/bow.py
+++ build_dataset/clickbait_direct/methods/bow.py
@@ -6,6 +6,9 @@ from konlpy.tag import Okt
 import re
 from tqdm.auto import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
+import torch
+import torch.nn.functional as F
+import pickle
 
 def bow_title_category_select(file_path: str,
                           sim_argmax: dict) -> str:
@@ -65,7 +68,7 @@ def morphs_extract(file_list: list, morphs_extract_path: str, morphs_type: str) 
     morphs_extracted = dict()
 
     print('extracting morphemes...')
-    RE_FILTER = re.compile("[\[.\],!?\"':;~()]")
+    RE_FILTER = re.compile("[\[.\],!?\"':;~()]") #&+-
 
     for file_path in tqdm(file_list):
         # load source file
@@ -117,6 +120,19 @@ def count_words(news_TitleOrContent:list) -> dict:
         bow[word]=bow_vector[word_to_index[word]]
     return bow
 
+def concat_bow(bowForADoc: dict):
+    """
+    concat bow
+    """
+    print('make bow vocab column')
+    vacab_list=[set(item.keys()) for item in tqdm(bowForADoc.values())]
+    columns= set.union(*vacab_list)
+    bow_total=pd.DataFrame(columns=columns)
+    print('concat bow')
+    for item in tqdm(bowForADoc.values()):
+        bow_total=bow_total.append(item,ignore_index=True).fillna(0)
+    return bow_total
+
 def make_bag_of_words(file_list: list, category: str, bow_dir: str, morphs_extract_path: str, morphs_type: str) -> dict:
     '''
     from extracted morphs, build bag of words
@@ -143,15 +159,16 @@ def make_bag_of_words(file_list: list, category: str, bow_dir: str, morphs_extra
         newsContent=newsContents[i].split()
         newsFile_path=newsFile_paths[i]       
         
-        
-        bow_titles[newsID] =count_words(newsTitle) #newsID 대신 newsFile_path를 넣어도 될 것 같다.
+        bow_titles[newsID] =count_words(newsTitle).
         bow_contents[newsID] = count_words(newsContent)
 
-    bow_titles_total=pd.DataFrame(bow_titles).T.fillna(0)
-    bow_contents_total=pd.DataFrame(bow_contents).T.fillna(0)
+    bow_titles_total=concat_bow(bow_titles)
+    bow_contents_total=concat_bow(bow_contents)
+    # bow_titles_total=pd.DataFrame(bow_titles).T.fillna(0)  #여기서 형태소 전부 사용의 SO의 경우 에러발생: 볼용어 제거후 사이즈 줄여서 해보자.
+    # bow_contents_total=pd.DataFrame(bow_contents).T.fillna(0)
 
-    bow_titles_total.to_csv(f'{bow_dir}/{morphs_type}/{category}_bow_titles_total.csv')
-    bow_contents_total.to_csv(f'{bow_dir}/{morphs_type}/{category}_bow_contents_total.csv')
+    bow_titles_total.to_pickle(f'{bow_dir}/{morphs_type}/{category}_bow_titles_total.pkl')
+    bow_contents_total.to_pickle(f'{bow_dir}/{morphs_type}/{category}_bow_contents_total.pkl')
 
     return bow_titles_total, bow_contents_total
 
@@ -161,26 +178,55 @@ def save_bow_sim_matrix(file_list: list, category: str, bow_dir: str, morphs_ext
     '''
 
     # make & save bow similarity matrix
-    if not os.path.exists(f'{bow_dir}/{morphs_type}/{category}_bow_titles_total.csv'):
-        bow_titles_total, bow_contents_total = make_bag_of_words(file_list, category, bow_dir, morphs_extract_path, morphs_type)
+
+    if not os.path.exists(f'{bow_dir}/{morphs_type}/{category}_bow_titles.npy') and not os.path.exists(f'{bow_dir}/{morphs_type}/{category}_bow_contents.npy'):
+        if not os.path.exists(f'{bow_dir}/{morphs_type}/{category}_bow_titles_total.pkl') and not os.path.exists(f'{bow_dir}/{morphs_type}/{category}_bow_contents_total.pkl'):
+            bow_titles_total, bow_contents_total = make_bag_of_words(file_list, category, bow_dir, morphs_extract_path, morphs_type)
+        else:
+            bow_titles_total = pd.read_pickle(f'{bow_dir}/{morphs_type}/{category}_bow_titles_total.pkl')
+            bow_contents_total = pd.read_pickle(f'{bow_dir}/{morphs_type}/{category}_bow_contents_total.pkl')
+        
+        # set device
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        with torch.no_grad():
+            bow_titles_total=torch.from_numpy(bow_titles_total.to_numpy()).to(device)
+            bow_contents_total=torch.from_numpy(bow_contents_total.to_numpy()).to(device)
+
+        # title
+        length = bow_titles_total.shape[0]
+        bow_titles_sim_matrix = torch.zeros((length, length), device=device)
+
+        for i in tqdm(range(length), desc="Get title Sim-list"):
+            titles_similarity = F.cosine_similarity(bow_titles_total[i], bow_titles_total, dim=-1)
+            bow_titles_sim_matrix[i] += titles_similarity
+            bow_titles_sim_matrix[i][i] = -1
+        bow_titles_sim_matrix = bow_titles_sim_matrix.cpu().numpy()
+        
+        # content
+        length = bow_contents_total.shape[0]
+        bow_contents_sim_matrix = torch.zeros((length, length), device=device)
+
+        for i in tqdm(range(length), desc="Get content Sim-list"):
+            contents_similarity = F.cosine_similarity(bow_contents_total[i], bow_contents_total, dim=-1)
+            bow_contents_sim_matrix[i] += contents_similarity
+            bow_contents_sim_matrix[i][i] = -1
+        bow_contents_sim_matrix = bow_contents_sim_matrix.cpu().numpy()
+
+        # bow_titles_sim_matrix = cosine_similarity(bow_titles_total)
+        # bow_contents_sim_matrix = cosine_similarity(bow_contents_total)
+
+        bow_titles_sim_path=f'{bow_dir}/{morphs_type}/{category}_bow_titles.npy'
+        bow_contents_sim_path=f'{bow_dir}/{morphs_type}/{category}_bow_contents.npy'
+
+        np.save(bow_titles_sim_path, bow_titles_sim_matrix)
+        np.save(bow_contents_sim_path, bow_contents_sim_matrix)
     else:
-        bow_titles_total = pd.read_csv(f'{bow_dir}/{morphs_type}/{category}_bow_titles_total.csv', index_col=0)
-        bow_contents_total = pd.read_csv(f'{bow_dir}/{morphs_type}/{category}_bow_contents_total.csv', index_col=0)
-
-    bow_titles_total=bow_titles_total.to_numpy()
-    bow_contents_total=bow_contents_total.to_numpy()
-    
-    bow_titles_sim_matrix = cosine_similarity(bow_titles_total)
-    bow_contents_sim_matrix = cosine_similarity(bow_contents_total)
-    bow_titles_sim_path=f'{bow_dir}/{morphs_type}/{category}_bow_titles.npy'
-    bow_contents_sim_path=f'{bow_dir}/{morphs_type}/{category}_bow_contents.npy'
-
-    np.save(bow_titles_sim_path, bow_titles_sim_matrix)
-    np.save(bow_contents_sim_path, bow_contents_sim_matrix)
+        bow_titles_sim_matrix = np.load(f'{bow_dir}/{morphs_type}/{category}_bow_titles.npy')
+        bow_contents_sim_matrix = np.load(f'{bow_dir}/{morphs_type}/{category}_bow_contents.npy')
 
     # save argmax of bow similarity matrix
-    bow_titles_sim_matrix[np.arange(bow_titles_sim_matrix.shape[0]), np.arange(bow_titles_sim_matrix.shape[0])] = -1
-    bow_contents_sim_matrix[np.arange(bow_contents_sim_matrix.shape[0]), np.arange(bow_contents_sim_matrix.shape[0])] = -1
+    # bow_titles_sim_matrix[np.arange(bow_titles_sim_matrix.shape[0]), np.arange(bow_titles_sim_matrix.shape[0])] = -1
+    # bow_contents_sim_matrix[np.arange(bow_contents_sim_matrix.shape[0]), np.arange(bow_contents_sim_matrix.shape[0])] = -1
     title_sim_argmax = bow_titles_sim_matrix.argmax(axis=1)
     content_sim_argmax = bow_contents_sim_matrix.argmax(axis=1)
 
