4af22ad4f979c09c851bbdd103f5d9b596cec699
tootouch <wogur379@gmail.com>
2022-12-12 18:08:57 +0900

    [direct_exp] remove bash

--- build_dataset/clickbait_direct/run.sh
+++
@@ -1,7 +0,0 @@
-# BoW_title_category
-# python split_data.py --yaml_config './configs/BoW_title_category.yaml'
-python build.py --yaml_config './configs/BoW_title_category.yaml'
-
-# BoW_content_category
-# python split_data.py --yaml_config './configs/BoW_content_category.yaml'
-python build.py --yaml_config './configs/BoW_content_category.yaml'
