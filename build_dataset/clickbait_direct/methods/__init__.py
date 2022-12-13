3b7489d03b8a1e506c3dec579c9f1a954aad8573
tootouch <wogur379@gmail.com>
2022-12-14 02:20:54 +0900

    [direct_exp] unified bow and tfidf

--- build_dataset/clickbait_direct/methods/__init__.py
+++ build_dataset/clickbait_direct/methods/__init__.py
@@ -1,3 +1,5 @@
 from .random import random_select, random_category_select
-from .tfidf import tfidf_title_category_select, tfidf_content_category_select
-from .bow import bow_title_category_select, bow_content_category_select
+from .tfidf import tfidf_title_category_select, tfidf_content_category_select, tfidf_sim_matrix
+from .bow import bow_title_category_select, bow_content_category_select, bow_sim_matrix
+from .ngram import ngram_title_category_select, ngram_content_category_select
+from .similarity import get_similar_filepath_dict
---
+++ build_dataset/clickbait_direct/methods/similarity.py
@@ -0,0 +1,94 @@
+import numpy as np
+import os
+import json
+
+from tqdm.auto import tqdm
+from konlpy.tag import Mecab
+from typing import List
+
+
+
+
+def get_similar_filepath_dict(make_sim_matrix_func, file_list: list, category_list: list, target: str, savedir: str) -> None:
+    # define save path
+    savepath = os.path.join(savedir, f'sim_index_{target}.json')
+
+    # load sim_filepath_dict
+    if not os.path.isfile(savepath):
+        sim_filepath_dict = dict()
+    else:
+        sim_filepath_dict = json.load(open(savepath, 'r'))
+
+    # define progress bar
+    pbar = tqdm(category_list, total=len(category_list))
+
+    for category in pbar:
+        pbar.set_description(f'Category: {category}')
+
+        # run if category not in sim_filepath_dict
+        if not category in sim_filepath_dict.keys():
+            # set default
+            sim_filepath_dict.setdefault(category, {})
+
+            # extract file path in category
+            file_list_cat = [f for f in file_list if category in f]
+
+            # load similarity matrix
+            sim_filepath_dict = extract_sim_filepath(
+                make_sim_matrix_func = make_sim_matrix_func,
+                file_list            = file_list_cat,
+                category             = category,
+                target               = target,
+                sim_filepath_dict    = sim_filepath_dict
+            )
+
+            # save sim_filepath_dict
+            json.dump(sim_filepath_dict, open(savepath, 'w'), indent=4)
+
+    return sim_filepath_dict
+
+
+def extract_sim_filepath(make_sim_matrix_func, file_list: list, category: str, target: str, sim_filepath_dict: dict) -> None:
+    """
+    extract filepath most similar to filepath1 using ngram similarity
+    """
+    
+    # extract nouns
+    nouns_list = extract_nouns(file_list=file_list, target=target)
+
+    # make similarity matrix
+    sim_matrix = make_sim_matrix_func(text=nouns_list)
+    sim_matrix[np.arange(sim_matrix.shape[0]), np.arange(sim_matrix.shape[0])] = -1
+    
+    # find argmax
+    sim_index = sim_matrix.argmax(axis=1)
+
+    # update sim_filepath_dict
+    for file_path, idx in zip(file_list, sim_index):
+        sim_filepath_dict[category][file_path] = file_list[idx]
+    
+    return sim_filepath_dict
+
+
+def extract_nouns(file_list: list, target: str) -> List[list]:
+    """
+    extract nouns from target text
+    """
+    # extract morphs
+    mecab = Mecab()
+
+    # define list
+    nouns_list = []
+
+    for file_path in tqdm(file_list, desc=f'Extract Morphs({target})', total=len(file_list), leave=False):
+        # load source file
+        source_file = json.load(open(file_path, "r"))
+
+        if target == 'title':
+            text = source_file['sourceDataInfo']['newsTitle']
+        elif target == 'content':
+            text = source_file['sourceDataInfo']['newsContent']
+
+        nouns_list.append(' '.join(mecab.nouns(text)))
+
+    return nouns_list
