8706a66e943ba365c8490e7d65bd8929f79df3da
tootouch <wogur379@gmail.com>
2022-12-15 21:49:02 +0900

    [task1] change BTS to BERT

--- task1_title/dataset/__init__.py
+++ task1_title/dataset/__init__.py
@@ -2,5 +2,5 @@ from .build_dataset import FakeDataset
 from .tokenizer import FNDTokenizer
 from .han import HANDataset
 from .fndnet import FNDNetDataset
-from .bts import BTSDataset
+from .bert import BERTDataset
 from .factory import create_tokenizer, create_dataloader, create_dataset, extract_word_embedding
