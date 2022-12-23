b733dc17faef8cf5bee94094c8f1efceaed7c0fe
tootouch <wogur379@gmail.com>
2022-12-23 16:19:01 +0900

    delete clickbait_direct

--- clickbait_direct/methods/__init__.py
+++
@@ -1,6 +0,0 @@
-from .random import random_select, random_category_select
-from .tfidf import tfidf_title_category_select, tfidf_content_category_select, tfidf_sim_matrix
-from .bow import bow_title_category_select, bow_content_category_select, bow_sim_matrix
-from .ngram import ngram_title_category_select, ngram_content_category_select, ngram_sim_matrix
-from .sentence_embedding import sentence_embedding_title_category_select, sentence_embedding_content_category_select, sentence_embedding_sim_matrix
-from .similarity import get_similar_filepath_dict, extract_nouns, extract_text
